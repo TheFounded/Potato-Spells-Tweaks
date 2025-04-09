3baa443c03d3365d8ad5f3173a2977bb3814574c
PotatoLady <sofia17.tb@gmail.com>
2025-04-09 01:22:17 -0300

    Initial Setup

--- src/main/java/net/potato_modding/potatospells/mixin/ISSMixin.java
+++ src/main/java/net/potato_modding/potatospells/mixin/ISSMixin.java
@@ -4,14 +4,13 @@ import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import io.redspace.ironsspellbooks.api.util.Utils;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(Utils.class)
 public class ISSMixin {
 
     @WrapMethod(method = "softCapFormula")
-    private static double SoftCapFormulaMixin(double x, Operation<Double> original) {
-         return x <= 1.75 ? x : 1 / (-16 * (x - 1.5)) + 2;
+    private static double SoftCapFormula(double x, Operation<Double> original) {
+        return x <= 1.0 ? x : 1.8-(8*0.1);
     }
 
 }
