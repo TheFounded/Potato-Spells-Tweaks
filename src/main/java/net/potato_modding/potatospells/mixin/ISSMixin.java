8e9a06e1d6965dae677f1e8520a989fea8282ff7
Ace The King <solcat0414@gmail.com>
2025-04-08 23:17:45 -0400

    Changelog:
    Cleaned up mixin so that there's no error lines. The rest is up to you babe <3
    
    - Ace

--- src/main/java/net/potato_modding/potatospells/mixin/ISSMixin.java
+++ src/main/java/net/potato_modding/potatospells/mixin/ISSMixin.java
@@ -1,17 +1,16 @@
 package net.potato_modding.potatospells.mixin;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import io.redspace.ironsspellbooks.api.util.Utils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Utils.class)
 public class ISSMixin {
 
-    @Inject(method = "softCapFormula", at = @At("RETURN"), cancellable = false)
-    private static void SoftCapFormulaMixin(double x, CallbackInfoReturnable<Double> cir) {
+    @WrapMethod(method = "softCapFormula")
+    private static double SoftCapFormulaMixin(double x, Operation<Double> original) {
          return x <= 1.75 ? x : 1 / (-16 * (x - 1.5)) + 2;
     }
 
