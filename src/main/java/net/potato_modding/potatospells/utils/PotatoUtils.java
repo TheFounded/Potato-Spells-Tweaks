59a650d84a5d5d2345d5be0ee84f6d845b303aa1
PotatoLady <sofia17.tb@gmail.com>
2025-04-25 13:18:42 -0300

    Fixed a smol mistake

--- src/main/java/net/potato_modding/potatospells/utils/PotatoUtils.java
+++ src/main/java/net/potato_modding/potatospells/utils/PotatoUtils.java
@@ -8,10 +8,10 @@ public class PotatoUtils {
     static String cduncap = ServerConfigs.COOLDOWN_UNCAP.get();
     public static double cooldownsoftcap(double x) {
         if(cduncap == "nerfed") {
-            return x <= 0 ? (2*x)+0.44426 : x <= 4.80999 ? 2*Math.sin(0.28*(x+0.8)) : 2;
+            return x <= 0 ? (2*x)+0.44426 : (x <= 4.80999 ? 2*Math.sin(0.28*(x+0.8)) : 2);
         }
         else if(cduncap == "apotheosis") {
-            return x <= 0 ? (3*x)+0.72137 : x <= 8.01198 ? 2*Math.sin(0.15*(x+2.46)) : 2;
+            return x <= 0 ? (3*x)+0.72137 : (x <= 8.01198 ? 2*Math.sin(0.15*(x+2.46)) : 2);
         }
         else {
             //This is the default formula | using 'else' in case user inputs something invalid
@@ -22,10 +22,10 @@ public class PotatoUtils {
     static String ctuncap = ServerConfigs.CAST_UNCAP.get();
     public static double castsoftcap(double x) {
         if(ctuncap == "nerfed") {
-            return x <= 0 ? (2*x)+0.44426 : x <= 4.80999 ? 2*Math.sin(0.28*(x+0.8)) : 2;
+            return x <= 0 ? (2*x)+0.44426 : (x <= 4.80999 ? 2*Math.sin(0.28*(x+0.8)) : 2);
         }
         else if(ctuncap == "apotheosis") {
-            return x <= 0 ? (3*x)+0.72137 : x <= 8.01198 ? 2*Math.sin(0.15*(x+2.46)) : 2;
+            return x <= 0 ? (3*x)+0.72137 : (x <= 8.01198 ? 2*Math.sin(0.15*(x+2.46)) : 2);
         }
         else {
             //This is the default formula | using 'else' in case user inputs something invalid
@@ -36,10 +36,10 @@ public class PotatoUtils {
     static String runcap = ServerConfigs.RESIST_UNCAP.get();
     public static double resistsoftcap(double x) {
         if(runcap == "nerfed") {
-            return x <= 0 ? (2*x)+0.44426 : x <= 4.80999 ? 2*Math.sin(0.28*(x+0.8)) : 2;
+            return x <= 0 ? (2*x)+0.44426 : (x <= 4.80999 ? 2*Math.sin(0.28*(x+0.8)) : 2);
         }
         else if(runcap == "apotheosis") {
-            return x <= 0 ? (3*x)+0.72137 : x <= 8.01198 ? 2*Math.sin(0.15*(x+2.46)) : 2;
+            return x <= 0 ? (3*x)+0.72137 : (x <= 8.01198 ? 2*Math.sin(0.15*(x+2.46)) : 2);
         }
         else {
             //This is the default formula | using 'else' in case user inputs something invalid
